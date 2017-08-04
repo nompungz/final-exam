83bf9b645ebe7043296ef3b669c0db6d0a265416
tanapat.so <tanapat.soontornlimsiri@gmail.com>
2017-08-04 17:09:20 +0700

    final exam 1 all case

--- src/main/java/Exam1.java
+++ src/main/java/Exam1.java
@@ -1,6 +1,80 @@
 public class Exam1 {
 
-    public long solution(Long n ){
-        return n ;
+    Long diff = 0l;
+
+    public Long solution(Long n ){
+        Long result = n ;
+        String max ="";
+//        while(check(result)){
+//
+//                result = result-1;
+//        }
+//        return result;
+        if (n>=10){
+            max = findMax(n);
+        }
+        else{
+            return n ;
+        }
+        result = Long.parseLong(max,10);
+        return result;
+    }
+
+    public String findMax(Long n) {
+        String m = n.toString();
+        String result = "";
+        char[] x = m.toCharArray();
+        boolean isCheck = true;
+        int first =0;
+        int second =0;
+
+        for (int i = 0; i < m.length() ; i++) {
+            if (isCheck) {
+                first = Integer.parseInt(m.charAt(i) + "");
+                second = Integer.parseInt(m.charAt(i + 1) + "");
+                if (first >= second) {
+                    isCheck = false;
+                    if (second == 0){
+                        result = result+String.valueOf(first-1);
+                    }
+                    else{
+                        result = result+String.valueOf(first-1);
+                    }
+                }
+                else{
+                    result = result + m.charAt(i);
+                }
+            }
+            else{
+                result += "9";
+            }
+        }
+//        result = String.valueOf(x);
+        return result;
+    }
+
+    public boolean check(Long n) {
+//        String  m = n.toString();
+        boolean isCheck = false;
+//        if (n <10){
+//            return isCheck;
+//        }
+//        else{
+//            for (int i = 0; i < m.length()-1 ; i++) {
+//                int first = Integer.parseInt(m.charAt(i)+"");
+//                int second = Integer.parseInt(m.charAt(i+1)+"");
+//                if (first >=second ){
+//                    isCheck = true;
+//                    break;
+//                }
+//
+//            }
+//        }
+        return isCheck;
+
+    }
+    public int checkLength(Long n) {
+        String m = n.toString();
+        return m.length();
     }
 }
--- src/test/java/ExamTest1.java
+++ src/test/java/ExamTest1.java
@@ -1,4 +1,3 @@
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -9,51 +8,56 @@ public class ExamTest1 {
 
     @Test
     public void Test1(){
-        Long actualResult = 8l;
-        Long expectedResult = exam.solution(8l);
+        Long actualResult = exam.solution(8l);
+        Long expectedResult = 8l;
         assertEquals(expectedResult, actualResult);
     }
 
-    @Test 
+    @Test
     public void Test2(){
-        Long actualResult = 9l;
-        Long expectedResult = exam.solution(9l);
+        Long actualResult = exam.solution(9l);
+        Long expectedResult = 9l;
         assertEquals(expectedResult, actualResult);
     }
 
-    @Test @Ignore
+    @Test
     public void Test3(){
-        Long actualResult = 19l;
-        Long expectedResult = exam.solution(20l);
+        Long actualResult = exam.solution(20l);
+        Long expectedResult = 19l;
+//        String  m = exam.findMax(20l);
+//        assertEquals("20", m);
         assertEquals(expectedResult, actualResult);
     }
 
-    @Test @Ignore
+    @Test
     public void Test4(){
-        Long actualResult = 129l;
-        Long expectedResult = exam.solution(132l);
+        Long actualResult = exam.solution(132l);
+        Long expectedResult = 129l;
+//                String  m = exam.findMax(132l);
+//        assertEquals("130", m);
         assertEquals(expectedResult, actualResult);
     }
 
-    @Test @Ignore
+
+    @Test
     public void Test5(){
-        Long actualResult = 999999999l;
-        Long expectedResult = exam.solution(1111111110l);
+        Long actualResult = exam.solution(111111111111111110l);
+        Long expectedResult = 99999999999999999l;
         assertEquals(expectedResult, actualResult);
     }
 
-    @Test @Ignore
+    @Test
     public void Test6(){
-        Long actualResult = 8l;
-        Long expectedResult = exam.solution(8l);
+        Long actualResult = exam.solution(989999999999999999l);
+        Long expectedResult = 899999999999999999l;
         assertEquals(expectedResult, actualResult);
     }
 
 
-    @Test @Ignore
+    @Test
     public void Test7(){
-        Long actualResult = 8l;
-        Long expectedResult = exam.solution(8l);
+        Long actualResult = exam.solution(653161544328834085l);
+        Long expectedResult = 599999999999999999l;
         assertEquals(expectedResult, actualResult);
     }
 }
